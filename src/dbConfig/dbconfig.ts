c7792698442be2caee8e2d609d37008ef19cdb85
Rahul <rahul.chaurasiya.29.3@mountblue.tech>
2024-12-03 19:13:31 +0530

    remove es-lint errors

--- src/dbConfig/dbconfig.ts
+++ src/dbConfig/dbconfig.ts
@@ -2,8 +2,13 @@ import mongoose from "mongoose";
 
 export async function connect(){
     try {
-        mongoose.connect(process.env.MONGO_URI!)
-        const connection = mongoose.connection
+        
+        if (!process.env.MONGO_URI) {
+            throw new Error("MONGO_URI is not defined in environment variables.");
+        }
+
+        mongoose.connect(process.env.MONGO_URI)
+        const connection = mongoose.connection;
 
         connection.on('connected', () => {
             console.log("Mongo DB Connected");            
