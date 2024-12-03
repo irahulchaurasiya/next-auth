d839d34025a8f16ab966a07998db4ae6953e8831
Rahul <rahul.chaurasiya.29.3@mountblue.tech>
2024-12-03 18:53:11 +0530

    add next-auth demo project

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,28 @@
+import {connect} from '@/dbConfig/dbconfig';
+import User from '@/models/userModel';
+import {NextRequest, NextResponse} from 'next/server';
+import { getDataFromToken } from '@/helpers/getDataFromToken';
+
+connect();
+
+export async function POST(request: NextRequest) {
+    try {
+        //extract data from token
+        const userId = await getDataFromToken(request);
+
+        const user = await User.findOne({_id : userId}).select("-password");
+
+        // Check if there is no user
+        if(!user){
+            return NextResponse.json({error : "No user found"} , {status: 400})
+        }
+
+        return NextResponse.json({
+            message: "User Found",
+            data: user
+        })
+
+    } catch (error) {
+        
+    }
+}
---
+++ src/helpers/mailer.ts
@@ -0,0 +1,55 @@
+import User from '@/models/userModel';
+import nodemailer from 'nodemailer'
+import bcryptjs from 'bcryptjs';
+
+export const sendEmail = async({email, emailType, userId}:any) => {
+    try {
+
+      const hashedToken = await bcryptjs.hash(userId.toString(),10);
+
+        if(emailType === "VERIFY"){
+          await User.findByIdAndUpdate(userId, {
+            $set:{
+            verifyToken : hashedToken,
+            verifyTokenExpiry: (Date.now() + 3600000)
+            }
+          });
+
+        }
+        else if(emailType === "RESET"){
+          await User.findByIdAndUpdate(userId, {
+            $set: {
+            forgotPasswordToken : hashedToken,
+            forgotPasswordTokenExpiry: (Date.now() + 3600000)
+            }
+          });
+        }
+      
+        // Looking to send emails in production? Check out our Email API/SMTP product!
+          var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: "09c4a556f9cc44", // should not be here should be in env file
+              pass: "523c93fe014b9c" // should not be here
+            }
+          });
+
+          const mailOptions = {
+            from: 'rahul@gmail.com', 
+            to: email, 
+            subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+            html: `<p> Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser.
+            <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            </p>`,
+          }
+
+          const mailResponse = await transport.sendMail(mailOptions);
+
+          return mailResponse;
+
+    } catch (error:any) {
+        throw new Error(error.message);
+    }
+}
